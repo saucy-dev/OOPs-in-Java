e5d384bb921083b680dd00d3574589df412dc71c
saucy-dev <ayushsaroj140@gmail.com>
2025-09-18 14:41:50 +0530

    ok

---
+++ ListDemo.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+public class ListDemo {
+    public static void main(String[] args) {
+        List<String> students = new ArrayList<>();
+        System.out.println("-----Methods from Collection Interface");
+        students.add("Kavin");
+        students.add("Rohit");
+        students.add("Priya");
+        System.out.println("Students: "+ students);
+
+        List<String> newStudents = new ArrayList<> ();
+        newStudents.add("Amit");
+        
+    }
+}
